3afc66d81b54fdcb8fc84e69e56428902ea2e182
JoaNav7 <71232713+JoaNav7@users.noreply.github.com>
2020-09-13 17:27:28 -0300

    Add files via upload

---
+++ Love/src/love/Love.java
@@ -0,0 +1,18 @@
+
+package love;
+
+/**
+ *
+ * @author Joaquin Navarro
+ */
+public class Love {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frame unFrame = new Frame();
+        unFrame.setVisible(true);
+    }
+    
+}
